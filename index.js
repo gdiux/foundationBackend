786a9debc72efa943d13ba8e4a8460e71d19bedc
Gilmer Contreras <adgdiux@gmail.com>
2023-12-08 21:07:23 -0400

    Finalizacion 08122023

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const cors = require('cors');
 
 // DATABASE
-const { dbConection } = require('./databases/config');
+// const { dbConection } = require('./databases/config');
 
 // SERVER EXPRESS
 const app = express();
@@ -20,13 +20,13 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 // CONNECT DATABASE
-dbConection();
+// dbConection();
 
 // PUBLIC FOLDER
 app.use(express.static('public'));
 
 // RUTAS
-app.use('/api/user', require('./routes/user.routes'));
+// app.use('/api/user', require('./routes/user.routes'));
 
 // SPA
 app.get('*', (req, res) => {
